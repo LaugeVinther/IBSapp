3f2f006dea6113120b3954fbcf7e4b31273fc403
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2018-11-01 13:06:01 +0100

    Ændret START/STOP tilbage til det originale

--- IBSapplication/IBSapplication/Form1.Designer.cs
+++ IBSapplication/IBSapplication/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label9 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
@@ -202,7 +202,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(244, 118);
             this.button5.TabIndex = 30;
-            this.button5.Text = "START /  ";
+            this.button5.Text = "START /  STOP";
             this.button5.UseVisualStyleBackColor = false;
             // 
             // label3
@@ -306,29 +306,29 @@
             // chart1
             // 
             this.chart1.BackColor = System.Drawing.Color.Black;
-            chartArea1.AxisX.Title = "Time (ms)";
-            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.Lime;
-            chartArea1.AxisY.Title = "Pressure (mmHg)";
-            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Lime;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.AxisX.Title = "Time (ms)";
+            chartArea2.AxisX.TitleForeColor = System.Drawing.Color.Lime;
+            chartArea2.AxisY.Title = "Pressure (mmHg)";
+            chartArea2.AxisY.TitleForeColor = System.Drawing.Color.Lime;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(11, 10);
             this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chart1.Name = "chart1";
-            series1.BorderColor = System.Drawing.Color.Black;
-            series1.BorderWidth = 3;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Color = System.Drawing.Color.Lime;
-            series1.LabelBackColor = System.Drawing.Color.Black;
-            series1.LabelBorderColor = System.Drawing.Color.Black;
-            series1.LabelForeColor = System.Drawing.Color.Lime;
-            series1.Legend = "Legend1";
-            series1.Name = "Blood Pressure";
-            this.chart1.Series.Add(series1);
+            series2.BorderColor = System.Drawing.Color.Black;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Lime;
+            series2.LabelBackColor = System.Drawing.Color.Black;
+            series2.LabelBorderColor = System.Drawing.Color.Black;
+            series2.LabelForeColor = System.Drawing.Color.Lime;
+            series2.Legend = "Legend1";
+            series2.Name = "Blood Pressure";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(803, 299);
             this.chart1.TabIndex = 46;
             this.chart1.Text = "Blood Pressure";
