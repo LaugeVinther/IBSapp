6fb63c886673ed75ca090488dc8215068b6a6f74
Jenny Tran <jennytran@live.dk>
2018-11-01 14:47:13 +0100

    Jeg har ændret klassen Form til "PrimaryForm"

--- IBSapplication/IBSapplication/PrimaryForm.cs
+++ IBSapplication/IBSapplication/PrimaryForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace IBSapplication
 {
-    public partial class Form1 : Form
+    public partial class PrimaryForm : Form
     {
-        public Form1()
+        public PrimaryForm()
         {
             InitializeComponent();
         }
