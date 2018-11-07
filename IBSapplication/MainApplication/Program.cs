ef766002b204a10eb851f05d44d29032ed6b2fb7
Jenny Tran <jennytran@live.dk>
2018-11-07 13:01:54 +0100

    Oprettet alle klasser

--- IBSapplication/MainApplication/Program.cs
+++ IBSapplication/MainApplication/Program.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLogic;
+using BuisnessLogic;
+using PresentationLogic;
 
 namespace MainApplication
 {
     class Program
     {
+
         static void Main(string[] args)
         {
         }
