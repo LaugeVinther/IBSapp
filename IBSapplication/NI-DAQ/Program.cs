a6240d6d6f02b9180680c4f807fd8036f4688d5f
Jenny Tran <jennytran@live.dk>
2018-11-07 09:13:14 +0100

    Oprettelse af DataCollection

---
+++ IBSapplication/NI-DAQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NI_DAQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
