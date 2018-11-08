20edf83ff3936f80b58f80259ef3862fd1263fdc
Esmaralda Bjerrum <201708266@post.au.dk>
2018-11-08 08:37:30 +0100

    Ændringer på datalag Slettet programklasse i interfaces Ændringer i NI-DAQ-klassen

--- IBSapplication/DTOLogic/DTO_MeasuredData.cs
+++ IBSapplication/DTOLogic/DTO_MeasuredData.cs
@@ -8,5 +8,18 @@ namespace DTOLogic
 {
     public class DTO_MeasuredData
     {
+        public List<double> RawData { get; set; }
+
+        public  List<double> ConvertedData { get; set; }
+
+        public List<double> FilteredData { get; set; }
+
+        public int SystolicBP { get; set; }
+
+        public int DiastolicBP { get; set; }
+
+        public int AvgBP { get; set; }
+
+        public int Pulse { get; set; }
     }
 }
