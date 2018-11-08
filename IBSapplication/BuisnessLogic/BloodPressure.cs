f352b3fd52d57893086067ca46acd56dd82ec2d6
Esmaralda Bjerrum <201708266@post.au.dk>
2018-11-08 15:33:33 +0100

    Oprettelse af interface for nulpunkt og blodtryk. Ændringer i Datalag.

--- IBSapplication/BuisnessLogic/BloodPressure.cs
+++ IBSapplication/BuisnessLogic/BloodPressure.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Interfaces;
 
 namespace BuisnessLogic
 {
-    class BloodPressure
+    class BloodPressure : IBloodPressure
     {
+        public int CalculateAverage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalculateSystolic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalculateDiastolic()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- IBSapplication/BuisnessLogic/ZeroPointAdjustment .cs
+++ IBSapplication/BuisnessLogic/ZeroPointAdjustment .cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Interfaces;
 
 namespace BuisnessLogic
 {
-    class ZeroPointAdjustment
+    class ZeroPointAdjustment : IZeroPointAdjustment
     {
+        public void Adjust()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
