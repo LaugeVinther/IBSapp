a73e13d8e0eeb458950b18703767cbf9c3943244
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2018-11-08 15:24:41 +0100

    Ændringer i planen i Pulse

--- IBSapplication/BuisnessLogic/Pulse.cs
+++ IBSapplication/BuisnessLogic/Pulse.cs
@@ -11,12 +11,14 @@ namespace BusinessLogic
     {
        public void CalculatePulse()
        {
-          //Brug event-handler-delegation halløj til hele tiden at få den seneste liste fra ProcessedDataCollecter
+          //Brug event-handler-delegation halløj til hele tiden at få den seneste liste fra ProcessedDataCollecter 
+          //Eller også ligger dette ovenfor i Datacontroller
           //Lav liste over de sidste 10 sekunder
           //Lav FFT på listen over de sidste 10 sekunder. 
           //Find grundfrekvensen
           //Bestem perioden for grundfrekvensen
           //Omregn periden til puls (find ud af sammenhængen?)
+          //Skriv pulsen ind i DTO_Pulse
 
        }
     }
