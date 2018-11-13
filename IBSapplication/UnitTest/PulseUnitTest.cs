c523b2002604188df100da72da8cc1edb3d06cec
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2018-11-13 14:44:24 +0100

    Arbejdet med UnitTest af Pulse

---
+++ IBSapplication/UnitTest/PulseUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using NUnit.Framework;
+
+
+namespace UnitTest
+{
+   [TestFixture]
+    public class PulseUnitTest
+   {
+      private Pulse uut;
+
+      [SetUp]
+      public void SetUp()
+      {
+         uut = new Pulse();
+      }
+
+      [Test]
+      public void CalculatePulse_PositivNumbers_ExpectPulseIsX()
+      {
+         double [] positivMeasurements = new double[]{30,100,30,100,30,100,30,100,30};
+         int fs=2;
+         uut.CalculatePulse(positivMeasurements,fs);
+         Assert.That(uut._dtoPulse.Pulse, Is.EqualTo(90));
+      }
+
+    }
+}
