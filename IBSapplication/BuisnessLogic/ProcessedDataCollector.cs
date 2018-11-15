8cc3de677cf67e729c5ff8950d5004fa6c9b8122
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2018-11-15 16:07:10 +0100

    Oprettet DataProcessing, ProcessedDataCollector og DTO_mmHg og ændret lidt i DTO_mV.

---
+++ IBSapplication/BuisnessLogic/ProcessedDataCollector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTOLogic;
+
+namespace BusinessLogic
+{
+    class ProcessedDataCollector
+    {
+        DTO_mmHg mmHgDTO = new DTO_mmHg();
+        List<DTO_mmHg> ProcessedData = new List<DTO_mmHg>();
+
+        public void collectData()
+        {
+            ProcessedData.Add(mmHgDTO);
+        }
+    }
+}
