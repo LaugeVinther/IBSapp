db59d4f8f513b51e6e9b0f750a7ad736ba7467a3
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2018-11-15 15:23:14 +0100

    Arbejdet med Pulse og Bloodpressure (samt disses DTO'er og Interfaces). Desuden også Unittest af Pulse

--- IBSapplication/UnitTest/PulseUnitTest.cs
+++ IBSapplication/UnitTest/PulseUnitTest.cs
@@ -23,10 +23,20 @@ namespace UnitTest
       [Test]
       public void CalculatePulse_21PositivNumbers_ExpectPulseIs28()
       {
-         double [] positivMeasurements = new double[]{30,100,30,100,30,100,30,100,30,100,30, 100, 30, 100, 30, 100, 30, 100, 30,100,30};
-         double fs=1;
+         List<double> sinusList = new List<double>();
+
+         for (int i = 0; i < 50; i++)
+         {
+            double f;
+            f = 50*Math.Sin(2 * Math.PI * (i*0.02));
+            sinusList.Add(f);
+         }
+
+
+         double[] positivMeasurements = sinusList.ToArray();
+         double fs=50;
          uut.CalculatePulse(positivMeasurements,fs);
-         Assert.That(uut._dtoPulse.Pulse, Is.EqualTo(28));
+         Assert.That(uut._dtoPulse.Pulse, Is.EqualTo(60));
       }
 
       [Test]
