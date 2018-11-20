9f021ecfa620858561beee52ed00a49f163e676a
Jenny Tran <jennytran@live.dk>
2018-11-20 12:53:23 +0100

    Ændringer i DataProcessing

--- IBSapplication/BuisnessLogic/DataProcessing.cs
+++ IBSapplication/BuisnessLogic/DataProcessing.cs
@@ -4,18 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataLogic;
+using Interfaces;
 
 namespace BusinessLogic
 {
-    class DataProcessing
+    public class DataProcessing
     {
+        private DataLogicIF _dataobject;
+
         private bool isRunning;
+
+        public DataProcessing()
+        {
+            _dataobject = new DataCollection();
+        }
+
         public void Start()
         {
             while (isRunning = true)
             {
 
             }
+        }
+
+        public void GetCalibration ()
+        {
+            _dataobject.GetOneDataPoint();
         }
     }
 }
